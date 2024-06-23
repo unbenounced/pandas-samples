33679958c71d9ba0c821bac06d1cc479f531c8dd
Eric Martinez <159489361+unbenounced@users.noreply.github.com>
2024-06-23 02:15:31 -0400

    mean

---
+++ easy/post-likes.py
@@ -0,0 +1,11 @@
+# You are given a list of posts of a Facebook user. Find the average number of likes.
+
+# eric
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+fb_posts["no_of_likes"].mean()
+
+#strata
+result = fb_posts['no_of_likes'].mean()
