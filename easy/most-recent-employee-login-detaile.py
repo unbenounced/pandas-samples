1ce544d4a65c8a70f813e378f940fbf85ac2a75b
Eric Martinez <159489361+unbenounced@users.noreply.github.com>
2024-06-23 03:00:43 -0400

    group, max, to_frame, and drop

---
+++ easy/most-recent-employee-login-detaile.py
@@ -0,0 +1,26 @@
+#Amazon's information technology department is looking for information on employees' most recent logins.
+#The output should include all information related to each employee's most recent login.
+
+#eric 
+# Import your libraries
+import pandas as pd
+
+last_login = worker_logins.groupby('worker_id')["login_timestamp"].max().reset_index()
+worker_logins.merge(last_login, on=["worker_id", "login_timestamp"]).sort_values('worker_id')
+
+#strata
+import pandas as pd
+import numpy as np
+
+most_recent = (
+    worker_logins.groupby(["worker_id"])["login_timestamp"]
+    .max()
+    .to_frame("last_login")
+)
+result = pd.merge(
+    most_recent,
+    worker_logins,
+    how="inner",
+    left_on=["worker_id", "last_login"],
+    right_on=["worker_id", "login_timestamp"],
+).drop(columns=['last_login'])
